6c11029cb0c6e145708b1833a8809b9fb1cdc7a3
TEJ PRATAP <tejfaster55@gmail.com>
2021-02-13 20:03:54 +0530

    change in index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('message'));
-
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root') // <-- must match entry element in index.html
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
